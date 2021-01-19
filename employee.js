fdbe46c54bb282bf5423429d4031ea6969488afc
chavalk <chavalk@hotmail.com>
2021-01-19 14:39:22 -0600

    Added function for initial inquiry and function to get all employees.

--- employee.js
+++ employee.js
@@ -8,11 +8,40 @@ var connection = mysql.createConnection({
 
     user: "root",
 
-    password: "",
+    password: "Culorico19*",
     database: "employee_trackerDB"
 });
 
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
+    runSearch();
 });
+
+function runSearch() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View employees",
+                "View roles",
+                "View departments"
+            ]
+        })
+        .then(function(answer) {
+            switch (answer.action) {
+            case "View employees":
+                employeesSearch();
+                break;
+            }
+        });
+}
+
+function employeesSearch() {
+    var query = "SELECT e.id, first_name, last_name, title, salary FROM employee AS e LEFT JOIN role AS r ON e.role_id = r.id;";
+    connection.query(query, function(err, res) {
+        console.log(res);
+    })
+}
